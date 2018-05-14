c72ddceb23ca6baf04940d38ec4998261e91ed8e
JuanAntonioReyes <jantonio.reyes.navarro@gmail.com>
2018-05-15 00:34:31 +0200

    cors added

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser')
 
 const app = express();
+const cors = require('cors');
 app.use(bodyParser.json())
 app.use(express.urlencoded());
+app.use(cors());
 
 const serverPort = 3000;
 
